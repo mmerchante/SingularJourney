28ef26a1b8c9465ccf20f2bfbd5428ce7edde80c
Manuel Araoz <manuelaraoz@gmail.com>
2013-08-12 18:35:43 -0300

    bugfixin

--- Assets/Scripts/PickupController.cs
+++ Assets/Scripts/PickupController.cs
@@ -6,7 +6,6 @@ public class PickupController : MonoBehaviour {
 
     private GameObject heldObject;
     private bool itemHeld = false;
-    private float objectDist = -0.1f;
     private RaycastHit hit;
 
     void Start() {
@@ -14,19 +13,16 @@ public class PickupController : MonoBehaviour {
 
     void Update() {
         if (Input.GetMouseButtonDown(0)) {
-            Debug.Log("DOWN!");
             if (!itemHeld) {
-                Debug.Log("not itemheld");
                 int layerMask = 1 << 8;
                 if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 5.0f, layerMask)) {
-                    Debug.Log("raycast");
                     if (hit.collider.gameObject) {
                         heldObject = hit.collider.gameObject;
                         heldObject.transform.parent = gameObject.transform;
                         Debug.Log(heldObject.name);
                         heldObject.GetComponent<Rigidbody>().isKinematic = true;
 
-                        Vector3 handLocation = new Vector3(0, objectDist, 0);
+                        Vector3 handLocation = new Vector3(0, pickupDistance, 0);
                         heldObject.transform.localPosition = handLocation;
                         itemHeld = true;
                     }
