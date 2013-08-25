8f4e5467efb0bbca2d00a5ca515b62734eac6d3f
Mariano Merchante <merchante.mariano@gmail.com>
2013-08-25 13:58:35 -0300

    Mas prefabs de enviro!

--- Assets/Scripts/Effects/Oscillator.cs
+++ Assets/Scripts/Effects/Oscillator.cs
@@ -3,19 +3,25 @@ using System.Collections;
 
 public class Oscillator : MonoBehaviour {
 
-    public Vector3 direction = Vector3.one;
+    public Vector3 direction = Vector3.up;
     public float frequency = 1f;
     public float amplitude = 1f;
+    public float offset = 0f;
+
+    public bool randomOffset = true;
 
     private Vector3 initialPosition;
 
     void Start()
     {
+        if (randomOffset)
+            this.offset = Random.value * 2f * Mathf.PI;
+
         this.initialPosition = this.transform.localPosition;
     }
 
     void Update () 
     {
-        this.transform.localPosition = initialPosition + direction * amplitude * Mathf.Sin(frequency * Time.time);
+        this.transform.localPosition = initialPosition + direction * amplitude * Mathf.Sin(frequency * Time.time + offset);
 	}
 }
