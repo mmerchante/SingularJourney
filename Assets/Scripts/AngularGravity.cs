ee10fd58ece293e9c4e870c3b320a9b8c506875a
Manuel Araoz <manuelaraoz@gmail.com>
2013-08-25 23:39:04 -0300

    buggies

--- Assets/Scripts/AngularGravity.cs
+++ Assets/Scripts/AngularGravity.cs
@@ -3,12 +3,27 @@ using System.Collections;
 
 public class AngularGravity : MonoBehaviour {
 
+    private Vector3 gravity;
+
+    private float changeTime = 3f;
+    private float time;
 
     void Start() {
-
+        Randomize();
+        time = 0;
     }
 
     void Update() {
+        time += Time.deltaTime;
+        if (time > changeTime) {
+            time -= changeTime;
+            Randomize();
+        }
+        
+        rigidbody.AddTorque(gravity);
+    }
 
+    void Randomize() {
+        gravity = Random.onUnitSphere * 10f;
     }
 }
