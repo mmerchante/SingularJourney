0d0aa82ecc95fb5e8b297413dd63900c74b30754
Manuel Araoz <manuelaraoz@gmail.com>
2013-08-26 00:59:42 -0300

    music

---
+++ Assets/Scripts/MusicQueue.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MusicQueue : MonoBehaviour {
+
+    public AudioClip[] src;
+    public Queue<AudioClip> clips;
+
+    private AudioClip current;
+
+    void Start () {
+
+        clips = new Queue<AudioClip>(src);
+
+        current = clips.Dequeue();
+        audio.clip = current;
+        audio.Play();
+    }
+    
+    void Update () {
+        if (!audio.isPlaying) {
+            clips.Enqueue(current);
+            current = clips.Dequeue();
+            audio.clip = current;
+            audio.Play();
+        }
+    }
+}
